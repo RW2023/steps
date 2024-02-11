4b815f8806fe70af7b57427f212dc67a9ca2962b
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-02-10 23:47:17 -0500

    Update button styles and add active class to numbers

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ function app(){
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className="active">1</div>
+        <div className="active">2</div>
+        <div className="active">3</div>
       </div>
 
       <p className="message">Place Holder for Message</p>
 
       <div className="buttons">
-        <button style={{ backgroundColor: '#7950f2', color:'#fff' }}>Next</button>
-        <button style={{ backgroundColor: '#7950f2', color:'#fff' }}>Previous</button>
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+          Next
+        </button>
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+          Previous
+        </button>
       </div>
     </div>
   );
