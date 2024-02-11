15bfb18883efecf00f492d717ff952493e3b0ba0
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-02-10 23:36:58 -0500

    Add steps, message, and buttons to App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 function app(){
   return (
-    <div></div>
+    
+      <div className="steps">
+        <div className="numbers">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+      
+      <p className="message">Place Holder for Message</p>
+
+      <div className="buttons">
+        <button>Next</button>
+        <button>Previous</button>
+      </div>
+    </div>
   )
 }
 
